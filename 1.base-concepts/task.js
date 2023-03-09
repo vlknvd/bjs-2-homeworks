a0dee12b43815008d56e6e224de701ec5671c509
Darya <vlknvd@yandex.ru>
2023-03-09 21:01:14 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b*b - 4*a*c;
+  if (d > 0) {
+    return arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  } else if (d === 0) {
+    return arr = [-b/(2*a)];
+  } else if (d < 0) {
+    arr = [];
+    return arr;
+  };
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+      return false;
+      } else {
+        
+      }
+  let P, S, n, payment, sum;  
+        P = (percent / 100) / 12;
+        S = amount - contribution;
+        n = countMonths;
+        payment = S * (P + (P / (((1 + P)**n) - 1)));
+        sum = payment * n;
+        sum = Math.round(sum*100)/100;
+        return sum;
 }

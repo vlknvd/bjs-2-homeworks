46d575b875ddd35b4015eb9f5c884f4023473b9b
Darya <vlknvd@yandex.ru>
2023-03-10 19:01:28 +0300

    Edit task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
   let d = b*b - 4*a*c;
   if (d > 0) {
-    return arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+    return [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
   } else if (d === 0) {
-    return arr = [-b/(2*a)];
+    return [-b/(2*a)];
   } else if (d < 0) {
-    arr = [];
-    return arr;
+    return [];
   };
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
       return false;
-      } else {
-        
       }
-  let P, S, n, payment, sum;  
-        P = (percent / 100) / 12;
-        S = amount - contribution;
-        n = countMonths;
-        payment = S * (P + (P / (((1 + P)**n) - 1)));
-        sum = payment * n;
-        sum = Math.round(sum*100)/100;
-        return sum;
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let n = countMonths;
+  let payment = S * (P + (P / (((1 + P)**n) - 1)));
+  let sum = payment * n;
+      sum = Math.round(sum*100)/100;
+      return sum;
 }
